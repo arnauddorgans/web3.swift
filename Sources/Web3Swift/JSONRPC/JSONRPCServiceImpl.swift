aaafd7bfb8627d481b33a06d9db306845f27ce86
Arnaud Dorgans <arnaud.dorgans@gmail.com>
2022-09-04 21:02:06 +0200

    fix linux import

--- Sources/Web3Swift/JSONRPC/JSONRPCServiceImpl.swift
+++ Sources/Web3Swift/JSONRPC/JSONRPCServiceImpl.swift
@@ -2,6 +2,9 @@
 // 
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 final class JSONRPCServiceImpl: JSONRPCService {
   private let environmentService: EnvironmentService
@@ -20,8 +23,29 @@ final class JSONRPCServiceImpl: JSONRPCService {
     urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
     urlRequest.httpMethod = "POST"
     urlRequest.httpBody = try jsonCoderService.encode(request)
-    let data = try await URLSession.shared.data(for: urlRequest)
+    let data = try await URLSession.shared.request(urlRequest)
     let response: JSONRPCResponse<Response> = try jsonCoderService.decode(data.0)
     return try response.result.get()
   }
+}
+
+extension URLSession {
+    func request(_ urlRequest: URLRequest) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                if let error = error {
+                    continuation.resume(with: .failure(error))
+                } else if let data = data, let response = response {
+                    continuation.resume(with: .success((data, response)))
+                } else {
+                    continuation.resume(with: .failure(URLSessionError.unknown))
+                }
+            }
+            task.resume()
+        }
+    }
+}
+
+enum URLSessionError: Error {
+    case unknown
 }
